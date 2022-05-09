82881d786ea79278586c43c4a82c651915365911
rodrigo alvarez <rodrigoalvarez.dev@gmail.com>
2022-05-09 18:17:10 -0300

    Updates

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 document.addEventListener('click', e =>{
     if(e.target.classList.contains('gallery-item')){
         const src = e.target.getAttribute('src');
+        const title = e.target.dataset.name;
+        const desc = e.target.dataset.desc;
+        document.querySelector('.modal-footer').textContent = desc;
+        document.querySelector('.modal-title').textContent = title;
         document.querySelector('.modal-img').src = src;
         const myModal = new bootstrap.Modal(document.getElementById('gallery-modal'));
         myModal.show();
